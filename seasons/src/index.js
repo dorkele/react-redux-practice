4fc84dc92de5c12cd6c67047d75c1833795bc60a
dorkele <dora_tominic@hotmail.com>
2020-09-03 10:39:57 +0200

    state in class comp

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
+    //particular to JavaScript - automatically and instantly called before anything else
+    constructor(props) {
+        // we have to super!
+        super(props);
+        this.state = { lat: null };
+    }
+
+    // React says we have to define render!!
     render() {
         window.navigator.geolocation.getCurrentPosition(
             //if it succeeds, the first argument:
