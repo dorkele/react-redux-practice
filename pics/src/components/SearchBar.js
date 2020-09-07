a95b7ee5fcfd02ec3de903c60f749a98085c50d1
dorkele <dora_tominic@hotmail.com>
2020-09-07 18:53:10 +0200

    this in js

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,10 +3,15 @@ import React from "react";
 class SearchBar extends React.Component {
     state = { term: "" };
 
+    onFormSubmit(e) {
+        e.preventDefault();
+        console.log(this.state.term);
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         {/* on input change withouth parentheses because with means we would 
@@ -15,7 +20,9 @@ class SearchBar extends React.Component {
                             type="text"
                             value={this.state.term}
                             onChange={(e) =>
-                                this.setState({ term: e.target.value })
+                                this.setState({
+                                    term: e.target.value,
+                                })
                             }
                         />
                     </div>
