0680157feb95456a9133d1d1a84d3b1590c668f2
dorkele <dora_tominic@hotmail.com>
2020-09-07 17:49:08 +0200

    search bar

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    state = { term: "" };
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        {/* on input change withouth parentheses because with means we would 
+                        call it each time the component is rendered */}
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={(e) =>
+                                this.setState({ term: e.target.value })
+                            }
+                        />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
