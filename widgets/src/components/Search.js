f87ca4908c5247e45c0ba08e285d81a3ab67740e
dorkele <dora_tominic@hotmail.com>
2020-09-22 16:35:07 +0200

    search component

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -2,26 +2,103 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
-    const [term, setTerm] = useState("");
-    console.log("i run with every render");
+    const [term, setTerm] = useState("dora");
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
-        // method #1 for async await with useEffect
-        const search = async () => {
-            await axios.get("hjdhsk");
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
         };
-        search();
-
-        // method #2 IFEE
-        // (async () => {
-        //     await axios.get("hkhdslia");
-        // })();
-
-        //method #3 promises
-        // axios.get("hjskla").then((response) => {
-        //     console.log(response.data);
-        // });
     }, [term]);
+
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get(
+                "https://en.wikipedia.org/w/api.php",
+                {
+                    params: {
+                        action: "query",
+                        list: "search",
+                        origin: "*",
+                        format: "json",
+                        srsearch: debouncedTerm,
+                    },
+                }
+            );
+            setResults(data.query.search);
+        };
+        if (debouncedTerm) {
+            search();
+        }
+    }, [debouncedTerm]);
+
+    // useEffect(() => {
+    //     // method #1 for async await with useEffect
+    //     const search = async () => {
+    //         const { data } = await axios.get(
+    //             "https://en.wikipedia.org/w/api.php",
+    //             {
+    //                 params: {
+    //                     action: "query",
+    //                     list: "search",
+    //                     origin: "*",
+    //                     format: "json",
+    //                     srsearch: term,
+    //                 },
+    //             }
+    //         );
+    //         setResults(data.query.search);
+    //     };
+    //     if (term && !results.length) {
+    //         search();
+    //     } else {
+    //         const timeoutId = setTimeout(() => {
+    //             if (term) {
+    //                 search();
+    //             }
+    //         }, 500);
+
+    //         return () => {
+    //             clearTimeout(timeoutId);
+    //         };
+    //     }
+
+    //     // method #2 IFEE
+    //     // (async () => {
+    //     //     await axios.get("hkhdslia");
+    //     // })();
+
+    //     //method #3 promises
+    //     // axios.get("hjskla").then((response) => {
+    //     //     console.log(response.data);
+    //     // });
+    // }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div className="item" key={result.pageid}>
+                <div className="right floated content">
+                    <a
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">{result.title}</div>
+                    <span
+                        dangerouslySetInnerHTML={{ __html: result.snippet }}
+                    ></span>
+                </div>
+            </div>
+        );
+    });
 
     return (
         <div>
@@ -35,6 +112,7 @@ const Search = () => {
                     />
                 </div>
             </div>
+            <div className="ui celled list">{renderedResults}</div>
         </div>
     );
 };
