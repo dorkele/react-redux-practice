3f5546fba5de21f255d092d7dd80e66deba6b452
dorkele <dora_tominic@hotmail.com>
2020-09-22 13:56:24 +0200

    search component

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,7 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
     const [term, setTerm] = useState("");
+    console.log("i run with every render");
+
+    useEffect(() => {
+        // method #1 for async await with useEffect
+        const search = async () => {
+            await axios.get("hjdhsk");
+        };
+        search();
+
+        // method #2 IFEE
+        // (async () => {
+        //     await axios.get("hkhdslia");
+        // })();
+
+        //method #3 promises
+        // axios.get("hjskla").then((response) => {
+        //     console.log(response.data);
+        // });
+    }, [term]);
 
     return (
         <div>
