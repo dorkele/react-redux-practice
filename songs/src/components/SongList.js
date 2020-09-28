48d2b379ae92f8ff206f4b689610fbf1201bc263
dorkele <dora_tominic@hotmail.com>
2020-09-28 18:10:25 +0200

    songs

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
+    renderList() {
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button
+                            className="ui button primary"
+                            onClick={() => this.props.selectSong(song)}
+                        >
+                            Select
+                        </button>
+                    </div>
+                    <div className="content">{song.title}</div>
+                </div>
+            );
+        });
+    }
+
     render() {
         // this. props === {songs: state.songs}
-        return <div>Song List</div>;
+        return <div className="ui divided list">{this.renderList()}</div>;
     }
 }
 //state - all our data from the store
 const mapStateToProps = (state) => {
-    return { songs: state.song };
+    return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
