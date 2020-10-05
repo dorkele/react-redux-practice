18113180de1dd162d3a62749cadce48684b6528d
dorkele <dora_tominic@hotmail.com>
2020-10-05 12:44:16 +0200

    secrets

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,4 +1,5 @@
 import React from "react";
+import secrets from "../secrets.json";
 
 class GoogleAuth extends React.Component {
     state = { isSignedIn: null };
@@ -9,24 +10,52 @@ class GoogleAuth extends React.Component {
             // returns promise
             window.gapi.client
                 .init({
-                    clientId:
-                        "577052830159-f4qe84oj1e9i743a1buan7tg1ues40mg.apps.googleusercontent.com",
+                    clientId: secrets.clientId,
                     scope: "email",
                 })
                 .then(() => {
                     this.auth = window.gapi.auth2.getAuthInstance();
                     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                    this.auth.isSignedIn.listen(this.onAuthChange);
                 });
         });
     }
 
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    };
+
+    onSignIn = () => {
+        this.auth.signIn();
+    };
+
+    onSignOut = () => {
+        this.auth.signOut();
+    };
+
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
-            return <div>BO</div>;
+            return null;
         } else if (this.state.isSignedIn) {
-            return <div>signed in</div>;
+            return (
+                <button
+                    onClick={this.onSignOut}
+                    className="ui red google button"
+                >
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            );
         } else {
-            return <div>not signed in</div>;
+            return (
+                <button
+                    onClick={this.onSignIn}
+                    className="ui red google button"
+                >
+                    <i className="google icon" />
+                    Sign In with Google
+                </button>
+            );
         }
     }
 
