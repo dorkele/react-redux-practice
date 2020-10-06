7494bd36f78ae5498c74a9876b8b156c80dc332f
dorkele <dora_tominic@hotmail.com>
2020-10-06 11:13:58 +0200

    redux-form

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
+// renaming the specifically named reducer to make it a bit clearer
+import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
 
 export default combineReducers({
     auth: authReducer,
+    // very particular key for redux-form
+    form: formReducer,
 });
