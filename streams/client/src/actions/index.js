e67835a5a376dcab7db7ebed51ba000bb2cff6d8
dorkele <dora_tominic@hotmail.com>
2020-10-08 11:59:22 +0200

    stream edit

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -8,6 +8,7 @@ import {
     DELETE_STREAM,
 } from "./types";
 import streams from "../apis/streams";
+import history from "../history";
 
 export const signIn = (userId) => {
     return {
@@ -29,6 +30,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
     dispatch({ type: CREATE_STREAM, payload: response.data });
     // do some programmatic navigation to
     // get the user back to the root route
+    history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
@@ -38,19 +40,20 @@ export const fetchStreams = () => async (dispatch) => {
 };
 
 export const fetchStream = (id) => async (dispatch) => {
-    const response = await streams.get(`/streams/:${id}`);
+    const response = await streams.get(`/streams/${id}`);
 
     dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-    const response = await streams.put(`/streams/:${id}`, formValues);
+    const response = await streams.patch(`/streams/${id}`, formValues);
 
     dispatch({ type: EDIT_STREAM, payload: response.data });
+    history.push("/");
 };
 
 export const deleteStream = (id) => async (dispatch) => {
-    await streams.delete(`/streams/:${id}`);
+    await streams.delete(`/streams/${id}`);
 
     dispatch({ type: DELETE_STREAM, payload: id });
 };
---
+++ streams/client/src/history.js
@@ -0,0 +1,5 @@
+// history is a library installed automatically as a dependency
+// with react-router-dom
+
+import { createBrowserHistory } from "history";
+export default createBrowserHistory();
