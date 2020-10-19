98164da4b8fdf543634b6a5faf73b87c4bd3d455
dorkele <dora_tominic@hotmail.com>
2020-10-19 21:21:38 +0200

    aaaa

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -8,6 +8,7 @@ import StreamEdit from "./streams/StreamEdit";
 import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
 import history from "../history";
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
 const App = () => {
     return (
